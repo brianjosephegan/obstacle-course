e1b0593ee6a8615acc2ab2a8ff9d50f4d6ff0e76
Brian Egan <brianjoseph.egan@gmail.com>
2021-03-14 12:55:23 -0700

    Initial Player
    Add player object and movement script.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, 0f, zValue);
+    }
+}
