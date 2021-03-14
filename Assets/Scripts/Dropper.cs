22aa5bac991c58736d62e5dd6e6548be7f0f66b2
Brian Egan <brianjoseph.egan@gmail.com>
2021-03-14 15:01:12 -0700

    Drop Obstacle
    Create obstacle prefab that will drop after a specified time.

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField]
+    float timeToDrop = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeToDrop)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
