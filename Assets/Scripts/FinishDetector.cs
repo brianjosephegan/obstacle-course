01c218114abaa2d499e909ad4e8400803b10f65f
Brian Egan <brianjoseph.egan@gmail.com>
2021-03-18 18:01:57 -0700

    Level Loading
    Add LevelLoader script to handle loading next scene. Add script to Finish prefab to detect hit by Player and load next level upon detection. Add basic second Level for testing.

---
+++ Assets/Scripts/FinishDetector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishDetector : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            FindObjectOfType<LevelLoader>().LoadNextLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
